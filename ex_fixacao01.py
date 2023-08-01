e9076da71db72a4aa96f23ac22bf7b7a7fc6be5d
César Cláudio Cattermol <cc.catter@gmail.com>
2023-08-01 11:01:51 -0300

    Initial commit

---
+++ ex_fixacao01.py
@@ -0,0 +1,14 @@
+'''
+-Criar novo PROJETO no PyCharm chamado PraticaPython;
+-Criar neste projeto um arquivo python chamado ex_fixacao01;
+-Resolver a questão: Fazer umprograma que pergunte um valor em dólares e converta para Reais. Considere que 1 Dólar vale 4,78 reais;
+-Faça este novo projeto virar um repositório no seu github;
+Entregue na ativdade Teams (Atividade FTI 01) o LINK do seu repositório com esta atividade.
+'''
+
+real = float(input("valor em dólares"))
+
+dolar = 4.78
+
+
+print("O valor em reais é:", real * dolar)
